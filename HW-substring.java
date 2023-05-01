493edd651801cd4bf8f8b874aa54265d8f6892e0
3Maali <maalinasser700@gmail.com>
2023-05-02 01:06:50 +0300

    STAR using substring

---
+++ HW-substring.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+	String x = "*****";
+	
+	for(int i=0; i <7; i++){
+	    if(i<4){
+	        
+	        
+	        System.out.println(x.substring(4-i));
+	    }
+	    else
+	    System.out.println(x.substring(i-1));
+	    
+	}
+	
+	
+	
+	}
+}
