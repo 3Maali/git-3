9abe21c5fd001029e0b6b88b2fb15f872a67a9ff
3Maali <maalinasser700@gmail.com>
2023-05-02 06:42:12 +0300

    LAST change: modify IF statement

--- HW-substring.java
+++ HW-substring.java
@@ -1,20 +1,12 @@
 public class Main
 {
 	public static void main(String[] args) {
-	String x = "*****";
+	String x = "*";
 	
 	for(int i=0; i <7; i++){
 	    if(i<4){
-	        
-	        
-	        System.out.println(x.substring(4-i));
-	    }
-	    else
+	        System.out.println(x);
+			x=x+"*";
+	    } else
 	    System.out.println(x.substring(i-1));
-	    
-	}
-	
-	
-	
-	}
-}
+	    }}}
